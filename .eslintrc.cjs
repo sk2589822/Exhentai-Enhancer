ec2820ae716b1cabf16d5f2c85bff63a89ab16ad
JK <sk25898222@gmail.com>
2022-08-13 21:22:00 +0800

    chore: set vue rule to `vue/vue3-recommended`

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -6,7 +6,7 @@ module.exports = {
   },
   'extends': [
     'eslint:recommended',
-    'plugin:vue/vue3-essential',
+    'plugin:vue/vue3-recommended',
     'plugin:@typescript-eslint/recommended',
   ],
   'parser': 'vue-eslint-parser',
