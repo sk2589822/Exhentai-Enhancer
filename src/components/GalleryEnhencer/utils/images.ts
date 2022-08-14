1e04650ed6d77ebc87b0577e486e2fec3e4af345
JK <sk25898222@gmail.com>
2022-08-14 13:00:22 +0800

    fix(gallery): prevent failure if page count larger than 10

--- src/components/GalleryEnhencer/utils/images.ts
+++ src/components/GalleryEnhencer/utils/images.ts
@@ -1,22 +1,26 @@
 import { getElement, getElements, getDoc, delay } from '@/utils/commons'
 import { Logger } from '@/utils/logger'
 
+const logger = new Logger('Fetch All Images')
+
 /**
  * 若 gallery 的頁數超過1頁，在第1頁時，會依序載入後面的頁面 (間隔3秒)
  */
 async function fetchAllImages() {
-  const logger = new Logger('Fetch All Images')
+  if (!isFirstPage()) {
+    logger.log('Not first page, do nothing')
+    return
+  }
 
   logger.log('Start')
   const pageUrls = getPageUrls()
 
-  if (pageUrls.length === 0) {
-    logger.log('Only one page, do nothing')
+  if (!pageUrls) {
     return
   }
 
-  if (!isFirstPage()) {
-    logger.log('Not first page, do nothing')
+  if (pageUrls.length === 0) {
+    logger.log('Only one page, do nothing')
     return
   }
 
@@ -52,16 +56,21 @@ async function fetchAllImages() {
   }
 
   function getPageUrls() {
-    // TODO: 自己產連結而不是用抓的
-    const links = getElements('.ptb td:not(.ptds)') as NodeListOf<HTMLElement>
-    const indexes = [...links]
-    indexes.pop()
-    indexes.shift()
+    const lastPageElement = getElement('.ptt td:nth-last-child(2)')
+    if (!lastPageElement) {
+      logger.error('Get last page element failed')
+      return
+    }
 
-    return indexes.map(elem => {
-      const childNode = elem.children[0] as HTMLAnchorElement
-      return childNode.href
-    })
+    const pageCount = Number(lastPageElement.innerText)
+    if (pageCount === 1) {
+      return []
+    }
+
+    const { href } = window.location
+    return Array(pageCount - 1)
+      .fill('')
+      .map((_, index) => `${href}?p=${index + 1}`)
   }
 
   function appendImages(elements: NodeListOf<HTMLElement>) {
