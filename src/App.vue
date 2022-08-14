5f09c6e56f1229be873bd14034de428288763d6e
JK <sk25898222@gmail.com>
2022-08-14 16:30:15 +0800

    refactor(gallery): inject css by vue instead of pure js

--- src/App.vue
+++ src/App.vue
@@ -1,24 +1,29 @@
 <template>
-  <div>
-    <GalleryEnhencer />
-  </div>
+  <component :is="enhencer" />
 </template>
 
 <script setup lang="ts">
 import GalleryEnhencer from '@/components/GalleryEnhencer/GalleryEnhencer.vue'
 
-import initGalleryEnhencer from '@/scripts/GalleryEnhencer'
 import initMultipageViewerEnhencer from '@/scripts/MultipageViewerEnhencer'
-import { onMounted } from 'vue'
+import { computed, onMounted } from 'vue'
+
+const isMultipageViewer = /https:\/\/exhentai\.org\/mpv\/\w+\/\w+\//.test(window.location.href)
 
 onMounted(() => {
-  const isGallery = /https:\/\/exhentai\.org\/g\/\w+\/\w+\//.test(window.location.href)
-  const isMultipageViewer = /https:\/\/exhentai\.org\/mpv\/\w+\/\w+\//.test(window.location.href)
-
-  if (isGallery) {
-    initGalleryEnhencer()
-  } else if (isMultipageViewer) {
+  if (isMultipageViewer) {
     initMultipageViewerEnhencer()
   }
+})
+
+const enhencer = computed(() => {
+  const { href } = window.location
+  const isGallery = /https:\/\/exhentai\.org\/g\/\w+\/\w+\//.test(href)
+
+  if (isGallery) {
+    return GalleryEnhencer
+  }
+
+  return null
 })
 </script>
--- src/components/GalleryEnhencer/GalleryEnhencer.vue
+++ src/components/GalleryEnhencer/GalleryEnhencer.vue
@@ -22,3 +22,78 @@ fetchAllImages()
 setImagesContainerWheelEvent()
 </script>
 
+<style lang="scss">
+div#gmid {
+  width: 931px;
+}
+
+div#gd5 {
+  width: 158px;
+}
+
+.popup {
+  position: absolute;
+  top: -99999px;
+  right: 0;
+  padding: 20px;
+  text-align: center;
+  background-color: #34353b;
+  border: white solid 3px;
+  border-radius: 20px;
+  opacity: 0;
+  transition: opacity 0.3s;
+}
+
+.popup--show {
+  top: initial;
+  opacity: 1;
+}
+
+.popup a {
+  text-decoration: underline;
+}
+
+.toast-container {
+  position: absolute;
+  top: 10px;
+  right: 10px;
+  display: flex;
+  z-index: 100;
+  flex-direction: column;
+  gap: 10px;
+}
+
+.toast {
+  padding: 10px;
+  font-size: 16px;
+  background-color: gray;
+  opacity: 0;
+  z-index: 100;
+  animation: show-toast 5s linear;
+}
+
+.toast h1 {
+  margin: 0;
+}
+
+@keyframes show-toast {
+  0% {
+    top: 12px;
+    opacity: 0;
+  }
+
+  10% {
+    top: 10px;
+    opacity: 1;
+  }
+
+  80% {
+    opacity: 1;
+  }
+
+  100% {
+    opacity: 0;
+  }
+}
+</style>
+
