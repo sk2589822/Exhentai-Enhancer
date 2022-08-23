d6aae42a51fbb6074a1a601a452c4091eef12c0c
JK <sk25898222@gmail.com>
2022-08-23 21:38:30 +0800

    chore: release v1.1.3

---
+++ dist/exhentai-enhencer.meta.js
@@ -0,0 +1,10 @@
+// ==UserScript==
+// @name       exhentai-enhencer
+// @namespace  https://github.com/sk2589822/Exhentai-Enhencer
+// @version    1.1.3
+// @icon       https://vitejs.dev/logo.svg
+// @match      https://exhentai.org/g/*/*/
+// @match      https://exhentai.org/mpv/*/*/
+// @require    https://cdn.jsdelivr.net/npm/vue@3.2.37/dist/vue.global.prod.js
+// @grant      unsafeWindow
+// ==/UserScript==
