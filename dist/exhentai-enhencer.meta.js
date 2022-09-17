a2e8d4c9be3b773956c071a3036828e97c50300e
JK <sk25898222@gmail.com>
2022-09-17 21:55:54 +0800

    chore: release v1.2.9

--- dist/exhentai-enhencer.meta.js
+++ dist/exhentai-enhencer.meta.js
@@ -1,11 +1,14 @@
 // ==UserScript==
 // @name       exhentai-enhencer
 // @namespace  https://github.com/sk2589822/Exhentai-Enhencer
-// @version    1.2.8
+// @version    1.2.9
 // @icon       https://vitejs.dev/logo.svg
 // @match      https://exhentai.org/g/*/*
+// @match      https://e-hentai.org/g/*/*
 // @match      https://exhentai.org/mpv/*/*
+// @match      https://e-hentai.org/mpv/*/*
 // @match      https://exhentai.org/s/*/*
+// @match      https://e-hentai.org/s/*/*
 // @require    https://cdn.jsdelivr.net/npm/vue@3.2.38/dist/vue.global.prod.js
 // @require    https://unpkg.com/pkg-cdn/dist/vue-toastification/v2.0.0-rc.5.iife.js
 // @grant      unsafeWindow
