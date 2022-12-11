0f9f6222e90ff5fda4077ddc9380b8b0a12bb4bd
JK <sk25898222@gmail.com>
2022-12-11 16:49:19 +0800

    chore: release v1.4.3

--- dist/exhentai-enhancer.user.js
+++ dist/exhentai-enhancer.user.js
@@ -1,10 +1,16 @@
 // ==UserScript==
-// @name               exhentai-enhancer
+// @name               Exhentai Enhancer
+// @name:en            Exhentai Enhancer
+// @name:zh-TW         Exhentai Enhancer
+// @name:zh-CN         Exhentai Enhancer
 // @namespace          https://github.com/sk2589822/Exhentai-Enhancer
-// @version            1.4.2
+// @version            1.4.3
 // @author             sk2589822
-// @description        improve UX of gallery & multipage viewer
-// @description:zh-TW  改善 gallery 和 multipage viewer 的 UX
+// @description        improve UX of Gallery Page & Multi-Page Viewer
+// @description:en     improve UX of Gallery Page & Multi-Page Viewer
+// @description:zh-TW  改善 Gallery Page 和 Multi-Page Viewer 的使用者體驗
+// @description:zh-CN  改善 Gallery Page 和 Multi-Page Viewer 的使用者體驗
+// @license            MIT
 // @icon               https://vitejs.dev/logo.svg
 // @match              https://exhentai.org/*
 // @match              https://e-hentai.org/*
