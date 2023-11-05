babb9dc0e3df3f5b16365ead410f0a3b355cf4a7
JK <sk25898222@gmail.com>
2023-11-05 09:27:29 +0800

    chore: release v1.11.1

--- dist/exhentai-enhancer.user.js
+++ dist/exhentai-enhancer.user.js
@@ -4,7 +4,7 @@
 // @name:zh-TW         Exhentai Enhancer
 // @name:zh-CN         Exhentai Enhancer
 // @namespace          https://github.com/sk2589822/Exhentai-Enhancer
-// @version            1.11.0
+// @version            1.11.1
 // @author             sk2589822
 // @description        improve UX of Gallery Page, Multi-Page Viewer and Front Page
 // @description:en     improve UX of Gallery Page, Multi-Page Viewer and Front Page
@@ -1118,7 +1118,7 @@
         let nextIndex = firstVisibleItemIndex;
         if (Math.sign(event.deltaY) === 1 && boundingTop <= 0) {
           nextIndex++;
-        } else if (Math.sign(event.deltaY) === -1 && boundingTop >= 0) {
+        } else if (Math.sign(event.deltaY) === -1 && boundingTop >= -1) {
           nextIndex--;
         }
         if (nextIndex >= 0 && nextIndex < firstItemOfRows.length) {
